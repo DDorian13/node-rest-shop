156c4201a13c37e6757b8bd1bc5db503a666eec7
bitman07 <61799348+bitman07@users.noreply.github.com>
2020-10-15 00:46:27 +0200

    13 full

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
 
 
 const storage = multer.diskStorage({
@@ -61,7 +62,7 @@ router.get('/', (req, res, next) => {
         });
 });
 
-router.post('/',upload.single('productImage'), (req, res, next) => {
+router.post('/'/*,checkAuth*/,upload.single('productImage'), (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         title: req.body.title,
@@ -125,7 +126,7 @@ router.get('/:productId', (req, res, next) => {
         });
 });
 
-router.patch('/:productId', (req, res, next) => {
+router.patch('/:productId'/*,checkAuth*/,(req, res, next) => {
     const id = req.params.productId;
     const updateOps ={};
     for (const ops of req.body){
@@ -150,7 +151,7 @@ router.patch('/:productId', (req, res, next) => {
         });
 });
 
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:productId'/*,checkAuth*/,(req, res, next) => {
     const id = req.params.productId;
     Product.remove({_id: id})
         .exec()
